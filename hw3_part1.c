dc0896ed9b4dbe43f020087d03275892f7800958
atheerIb123 <69142170+atheerIb123@users.noreply.github.com>
2023-06-17 08:42:06 +0000

    Update hw3_part1.c

--- hw3_part1.c
+++ hw3_part1.c
@@ -20,7 +20,9 @@
 #define	ET_EXEC	2	//Executable file 
 #define	ET_DYN	3	//Shared object file 
 #define	ET_CORE	4	//Core file 
-
+#define STB_LOCAL 0
+#define SHN_UNDEF 0
+#define SHT_SYMTAB 2
 
 /* symbol_name		- The symbol (maybe function) we need to search for.
  * exe_file_name	- The file where we search the symbol in.
@@ -33,49 +35,115 @@
  */
 unsigned long find_symbol(char* symbol_name, char* exe_file_name, int* error_val) 
 {
-	bool symbol_flag = false; //indicates whether the symbol is found
-	FILE* exe_file = fopen(exe_file_name, "rb"); //open the file in binary mode for read only
-	
-	if (exe_file == NULL)
-	{
-		*error_val = -3; //Couldn't open the file in this case or the file is not an executable
-		return 0;
-	}
+    bool symbol_flag = false; // Indicates whether the symbol is found
+    FILE* exe_file = fopen(exe_file_name, "rb"); // Open the file in binary mode for read only
 
-	Elf64_Ehdr elf_header;
+    if (exe_file == NULL)
+    {
+        *error_val = -3; // Couldn't open the file in this case or the file is not an executable
+        return 0;
+    }
 
-	size_t elf_header_size = sizeof(Elf64_Ehdr);
-	fread(&elf_header, elf_header_size, 1, exe_file); //read one instance of size = eHdr_size from the file
+    Elf64_Ehdr elf_header;
 
-	
-	if (elf_header.e_type != ET_EXEC)
-	{
-		*error_val = -3;
-		fclose(exe_file);
-		return 0;
-	}
+    size_t elf_header_size = sizeof(Elf64_Ehdr);
+    fread(&elf_header, elf_header_size, 1, exe_file); // Read one instance of size = eHdr_size from the file
 
-	//All set to start finding the symbol_table
+    if (elf_header.e_type != ET_EXEC)
+    {
+        *error_val = -3;
+        fclose(exe_file);
+        return 0;
+    }
 
-	Elf64_Shdr section_header;
-	
-	size_t Shdr_size = sizeof(section_header);
-	long offset_of_Shdr = elf_header.e_shoff + elf_header.e_shentsize * elf_header.e_shstrndx; //calc the offest to section header names/section header string table
+    // All set to start finding the symbol table
 
-	fseek(exe_file, offset_of_Shdr, SEEK_SET);
-	fread(&section_header, Shdr_size, 1, exe_file); //update "Elf64_Shdr section_header"
+    Elf64_Shdr section_header;
 
-	fseek(exe_file, elf_header.e_shoff, SEEK_SET);
+    size_t Shdr_size = sizeof(section_header);
+    long offset_of_Shdr = elf_header.e_shoff + elf_header.e_shentsize * elf_header.e_shstrndx; // Calculate the offset to section header names/section header string table
 
-	
+    fseek(exe_file, offset_of_Shdr, SEEK_SET);
+    fread(&section_header, Shdr_size, 1, exe_file); // Update "Elf64_Shdr section_header"
 
+    fseek(exe_file, elf_header.e_shoff, SEEK_SET);
 
-	return 0;
+    // Find the section header string table
+
+    char *section_names = malloc(section_header.sh_size);
+    fseek(exe_file, section_header.sh_offset, SEEK_SET);
+    fread(section_names, section_header.sh_size, 1, exe_file);
+
+    // Find the symbol table section
+
+    Elf64_Shdr symbol_table_header;
+
+    for (int i = 0; i < elf_header.e_shnum; i++)
+    {
+        printf("\n%d\n", i);
+        fread(&symbol_table_header, Shdr_size, 1, exe_file);
+
+        if (symbol_table_header.sh_type == 2)
+            break;
+
+        fseek(exe_file, elf_header.e_shentsize - Shdr_size, SEEK_CUR);
+    }
+
+    if (symbol_table_header.sh_type != SHT_SYMTAB)
+    {
+        *error_val = -1; // Symbol table not found
+        printf("\nwassup\n");
+        fclose(exe_file);
+        free(section_names);
+        return 0;
+    }
+
+    // Read the symbol table entries
+
+    Elf64_Sym symbol;
+    size_t symbol_size = sizeof(symbol);
+
+    fseek(exe_file, symbol_table_header.sh_offset, SEEK_SET);
+
+    while (fread(&symbol, symbol_size, 1, exe_file))
+    {
+        char *symbol_name_str = section_names + symbol_table_header.sh_link + symbol.st_name;
+
+        if (strcmp(symbol_name_str, symbol_name) == 0)
+        {
+            symbol_flag = true;
+            break;
+        }
+    }
+
+    fclose(exe_file);
+    free(section_names);
+
+    if (!symbol_flag)
+    {
+        *error_val = -1; // Symbol not found
+        return 0;
+    }
+
+    if (ELF64_ST_BIND(symbol.st_info) == STB_LOCAL)
+    {
+        *error_val = -2; // Symbol is not global
+        return 0;
+    }
+
+    if (symbol.st_shndx == SHN_UNDEF)
+    {
+        *error_val = -4; // Symbol is global but will come from a shared library
+        return 0;
+    }
+
+    *error_val = 1; // Symbol is found and is global
+    return symbol.st_value;
 }
 
 int main(int argc, char *const argv[]) {
 	int err = 0;
-	unsigned long addr = find_symbol(argv[1], "./verySecretProgram.axf", &err);
+	unsigned long addr = find_symbol(argv[1], argv[2], &err);
 
 
 	if (err >= 0)
